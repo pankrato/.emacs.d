315a9dbe4158d181d14675aae306d9e9355287cc
Ivan <ivan-p@yandex.ru>
2020-12-31 11:12:28 +0200

    Create init.el

---
+++ init.el
@@ -0,0 +1,10 @@
+(load-theme 'deeper-blue)
+
+(setq auto-save-default nil)
+(setq auto-save-list-file-prefix nil)
+(setq make-backup-files nil)
+
+(setq default-directory "~/")
+(setq command-line-default-directory "~/")
+
+(global-set-key [C-tab] 'next-buffer)
